cf8dc0e5f208f3ac84ad37270026021d0222574f
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-10-27 21:41:05 -0300

    "Interação com Botão"

--- app/src/main/java/br/com/etecia/foodfast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfast/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.etecia.foodfast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Inserindo Barra de Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Instanciando uma Classe
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    Criar Métodos para Clicar em Icones do Menu
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mFav:
+                Toast.makeText()
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
