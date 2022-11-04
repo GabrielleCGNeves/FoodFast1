90e06632a4ecbe73d4f548e2336d01b1dd6cf845
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2022-11-03 21:57:42 -0300

    colocando o toolbar inferior

--- app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
+++ app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
@@ -1,8 +1,10 @@
 package br.com.etecia.foodfast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -30,6 +32,49 @@ public class MenuAppBarTop_Activity extends AppCompatActivity {
             }
         });
 
+        idToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
 
+                switch(item.getItemId()){
+
+                    case R.id.mFavorite:
+                        Toast.makeText(getApplicationContext(), "curtiu", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+        idToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.mBusca:
+                        Toast.makeText(getApplicationContext(), "buscar", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+        idToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch(item.getItemId()){
+
+                    case R.id.mMore:
+                        Toast.makeText(getApplicationContext(), "mais opções", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+                return false;
+            }
+        });
     }
 }
